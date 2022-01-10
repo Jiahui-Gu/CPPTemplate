81739793d00890760284c2fad9da0946b542c607
jiahuigu <jiahuigu@sjtu.edu.cn>
2022-01-10 20:14:59 +0800

    上传代码模板

---
+++ template.cpp
@@ -0,0 +1,35 @@
+
+/*************************************************
+Author: GuJiahui
+Date: 2022-01-10
+Description: CPPTemplate
+*************************************************/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::unordered_set;
+using std::stack;
+using std::queue;
+using std::priority_queue;
+using std::max;
+using std::min;
+using std::pair;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    cout<<"Hello World";
+    return 0;
+}
