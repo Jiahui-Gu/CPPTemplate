cbdb9cd36574cd77ec18b0d759daf2589697d95a
Jiahui-Gu <jiahuigu@microsoft.com>
2026-06-20 22:03:55 +0800

    Add 2D vector sort example

--- templates/sort.cpp
+++ templates/sort.cpp
@@ -43,11 +43,31 @@ struct cmp{
     }
 };
 
+struct cmpVector2D{
+    bool operator()(const vector<int>& a, const vector<int>& b){
+        // Sort by the first value in ascending order.
+        // If the first values are equal, sort by the second value in ascending order.
+        if(a[0] != b[0]){
+            return a[0] < b[0];
+        }
+        return a[1] < b[1];
+    }
+};
+
 int main(){
     vector<double> nums = {3.14, 1.41, 2.72, 4.67, 1.73, 1.32, 1.62, 2.58};
     sort(nums.begin(), nums.end(), cmp());
     for(int i = 0; i < nums.size(); i++){
         cout<<nums[i]<<' ';
     }
+
+    cout<<endl;
+
+    vector<vector<int>> points = {{2, 3}, {1, 5}, {2, 1}, {1, 2}};
+    sort(points.begin(), points.end(), cmpVector2D());
+    for(int i = 0; i < points.size(); i++){
+        cout<<points[i][0]<<' '<<points[i][1]<<endl;
+    }
+
     return 0; 
 }
